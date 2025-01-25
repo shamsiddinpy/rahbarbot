e83d3808c03afb407ec75355d257f4e8f05926b8
shamsiddindaminov <shamsiddinpython215@gmail.com>
2025-01-25 18:11:53 +0500

    requirements.txts

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
 FROM python:3.10-slim
 
 # Django uchun kerakli paketlarni o'rnatish
-RUN apt-get update && apt-get install -y libpq-dev && apt-get clean
+RUN apt-get update && apt-get install -y libpq-dev gcc python3-pip && apt-get clean
 
 # Ishchi katalogni o'rnatish
 WORKDIR /app
+
+# Muhit o'zgaruvchilarini sozlash
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
+
 # Requirementsni ko'chirish va o'rnatish
 COPY requirements.txt /app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --upgrade pip
+RUN --mount=type=cache,id=custom-pip,target=/root/.cache/pip pip install -r requirements.txt
 
 # Django loyihani ko'chirish
 COPY . /app/
@@ -17,5 +21,5 @@ COPY . /app/
 # Portni ochish
 EXPOSE 8000
 
-# Django migratsiya va serverni ishga tushirish uchun
-CMD python3 manage.py makemigrations && python3 manage.py migrate  && python manage.py runserver 0.0.0.0:8000
+# Django migratsiya va Gunicorn serverni ishga tushurish uchun
+CMD ["sh", "-c", "python manage.py makemigrations && python manage.py migrate && gunicorn --bind 0.0.0.0:8000 root.wsgi:application"]
--- root/urls.py
+++ root/urls.py
@@ -7,14 +7,14 @@ from root.settings import MEDIA_URL, MEDIA_ROOT, STATIC_URL, STATIC_ROOT
 from apps.views import UserCreateView
 
 # API va Swagger uchun URL yo‘llarini to‘g‘rilash
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('apps.urls')),  # apps.urls ichidagi yo‘llar
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+urlpatterns = ([
+                   path('admin/', admin.site.urls),
+                   path('api/', include('apps.urls')),  # apps.urls ichidagi yo‘llar
+                   path('api-auth/', include('rest_framework.urls')),
+                   path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+                   path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-]
-+ static(MEDIA_URL, document_root=MEDIA_ROOT) + static(STATIC_URL, document_root=STATIC_ROOT)
+               ]
+               + static(MEDIA_URL, document_root=MEDIA_ROOT) + static(STATIC_URL, document_root=STATIC_ROOT))
